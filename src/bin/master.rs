ed53da0019eed9a46f05143e3cbfeea8b0087929
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2022-12-18 15:53:45 +0200

    Minor refactoring

--- src/bin/master.rs
+++ src/bin/master.rs
@@ -1,110 +1,100 @@
-// use std::{ops::Deref, path};
-use std::{sync::Mutex, process::Output, string, collections::HashMap, hash::Hash};
+use std::collections::BTreeMap;
+use std::error::Error;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
 
-use actix::fut::future;
-use actix_web::{App, HttpServer, middleware::Logger, web::{self, Data}, HttpResponse, post };
-use actix_web::http::{StatusCode};
-use futures::{future::join_all, Future, stream::FuturesUnordered, StreamExt};
-use reqwest::{self, Response};
-// use actix-rt;
-// use actix_web::http::{header::ContentType, StatusCode};
+use actix_web::http::StatusCode;
+use actix_web::{
+    middleware::Logger,
+    post,
+    web::{self, Data},
+    App, HttpResponse, HttpServer,
+};
 use env_logger;
-// use tokio::task{spawn}
-use tokio::spawn;
-// #[path = "../messages.rs"]
+use futures::{stream::FuturesUnordered, StreamExt};
+use reqwest;
+use tokio;
+
+use rlog::common::{append_message, get_messages, MessagesAppData};
+use rlog::errors::WriteConcernNotSatisfiedError;
+use rlog::structs::{MasterMessageRequest, Message, MessageID, SecondaryMessageRequest};
+
 mod rlog;
-use rlog::messages::{Message, MessageID, get_messages, save_message};
-use serde::de::value::Error;
-use::std::sync::atomic::{AtomicUsize, Ordering};
 
+const SECONDARY_URLS: [&'static str; 2] = ["localhost:8081", "localhost:8082"];
+// const SECONDARY_URLS: [&str; 0] = [];
 
-const SECONDARY_URLS: [&str; 2] =["secondary:8081", "secondary2:8082"];
-const NUM_SECONDARIES: usize = SECONDARY_URLS.len();
-const SECONDARY_PATH: &str = "private/message";
+const SECONDARY_PATH: &'static str = "private/message";
 static GLOBAL_MESSAGES_COUNT: AtomicUsize = AtomicUsize::new(0);
 
-
 #[post("/public/message/")]
-async fn post_message(data: Data<Mutex<HashMap<MessageID, Message>>>, req: web::Json<Message>) -> HttpResponse {
-    let msg = req.into_inner();
-    let write_concern = msg.concern();
-    let mut msg_to_send = msg.clone();
-    msg_to_send.set_id(GLOBAL_MESSAGES_COUNT.fetch_add(1, Ordering::AcqRel));
-    let save_reponse = save_message(data, msg);
+async fn post_message(
+    data: MessagesAppData,
+    request: web::Json<MasterMessageRequest>,
+) -> Result<HttpResponse, Box<dyn Error>> {
+    let request = request.into_inner();
+    let write_concern = request.wc.0;
+    let message_id = MessageID(GLOBAL_MESSAGES_COUNT.fetch_add(1, Ordering::AcqRel));
+    let message_ptr = Arc::clone(&request.msg_ptr);
 
-    let client = reqwest::Client::new();
-    let mut responces = FuturesUnordered::new();
-    
-    let closure = |address: String| {
-        
-        let msg = msg_to_send.clone();
-        let client = client.clone();
-        let addr = address.clone();
-        
-        responces.push(
-            spawn(async move {
-            let url = format!("http://{}/{}/", addr, SECONDARY_PATH);
-             client.post(url).json(&msg).send().await 
-        }))
-    };
+    // append message on master
+    append_message(data, request.msg_ptr, message_id.clone());
 
-    SECONDARY_URLS.map(|address| closure(address.to_string()));
+    // send asynchronous requests to the secondaries
+    let mut req_futures = FuturesUnordered::new();
+    SECONDARY_URLS.map(|address| {
+        let msg_ptr = Arc::clone(&message_ptr);
+        let id = message_id.clone();
 
-    if let Err(_) = save_reponse {return HttpResponse::InternalServerError().body("body"); }
+        let future = tokio::spawn(async move {
+            let url = format!("http://{}/{}/", address, SECONDARY_PATH);
+            let client = reqwest::Client::new();
+            let secondary_request = SecondaryMessageRequest { msg_ptr, id };
+            client.post(url).json(&secondary_request).send().await
+        });
 
-    let mut nadded=1;
-    loop{
+        req_futures.push(future);
+    });
+
+    let mut nadded: usize = 1;
+    loop {
         if nadded == write_concern {
-            
             break;
         }
-        match responces.next().await {
+        match req_futures.next().await {
             Some(result) => {
-                if let Ok(response) = result {
-                    if response.is_ok() {
-                        nadded += 1;    
-                    }
-                } 
+                let response = result??;
+                nadded += (response.status() == StatusCode::OK) as usize;
             }
-            None => {break;}
+            None => {
+                break;
+            }
         }
     }
 
-    if nadded < write_concern{
-        HttpResponse::InternalServerError().body("body")
-    }else {
-        HttpResponse::Ok().body("body")
+    if nadded < write_concern {
+        return Err(Box::new(WriteConcernNotSatisfiedError::new(
+            write_concern,
+            nadded,
+        )));
     }
 
+    Ok(HttpResponse::Ok().body("Success"))
 }
 
-// async fn handle_responces<T>(resps:T, wc:i32) -> HttpResponse
-// {
-//     let mut nadded = 1;
-//     loop {
-//         match resps.next().await {
-//             Some(result) => 
-//             {
-
-//             }
-//             None => {break;}
-//         }
-//     }
-// }
-
-#[actix_web::main] // or #[tokio::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let msg_vec: Data<Mutex<HashMap<MessageID, Message>>> = Data::new(Mutex::new(HashMap::new()));
+    let app_data = Data::new(Mutex::new(BTreeMap::<MessageID, Arc<Message>>::new()));
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .service(post_message)
             .service(get_messages)
-            .app_data(Data::clone(&msg_vec))
+            .app_data(Data::clone(&app_data))
     })
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
--- src/bin/secondary.rs
+++ src/bin/secondary.rs
@@ -1,49 +1,60 @@
-use std::{sync::Mutex, collections::HashMap, hash::Hash};
-
-use actix_web::{App, HttpServer, HttpResponse, middleware::Logger, web::{self, Data}, post};
-use env_logger;
+use std::collections::BTreeMap;
 use std::env;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
+
 use actix::clock::sleep as async_sleep;
+use actix_web::{
+    middleware::Logger,
+    post,
+    web::{self, Data},
+    App, HttpResponse, HttpServer,
+};
+use env_logger;
+use log::info;
+use rand::seq::SliceRandom;
+
+use rlog::common::{append_message, get_messages, MessagesAppData};
+use rlog::structs::{Message, MessageID, SecondaryMessageRequest};
 
 mod rlog;
-use rlog::messages::{Message, get_messages, save_message, MessageID };
-use rand::seq::SliceRandom;
-use log::info;
-
-
 
 #[post("/private/message/")]
-async fn post_message(data: Data<Mutex<HashMap<MessageID, Message>>>, req: web::Json<Message>) -> HttpResponse {
-    let msg = req.into_inner();
-    let sleeps:Vec<u64> = vec![1000,5000, 10000];
-    let sleep_dur = sleeps.choose(&mut rand::thread_rng()).unwrap_or(&10u64).clone();
+async fn post_message(
+    data: MessagesAppData,
+    request: web::Json<SecondaryMessageRequest>,
+) -> HttpResponse {
+    let request = request.into_inner();
+    let sleeps: Vec<u64> = vec![1000, 5000, 10000];
+    let sleep_dur = sleeps
+        .choose(&mut rand::thread_rng())
+        .unwrap_or(&10u64)
+        .clone();
     info!("Sleeping for: {:?}", sleep_dur);
-    
+
     async_sleep(Duration::from_millis(sleep_dur)).await;
-    
-    if let Err(_) = save_message(data, msg) { return  HttpResponse::InternalServerError().body("body"); }
-    HttpResponse::Ok().body("")
+
+    append_message(data, request.msg_ptr, request.id);
+    HttpResponse::Ok().body("Success")
 }
 
-
-
-#[actix_web::main] // or #[tokio::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    
+
     let args: Vec<String> = env::args().collect();
     let port = args[1].parse::<u16>().unwrap();
-    let map : HashMap<MessageID, Message> = HashMap::new();
-    let msg_vec: Data<Mutex<HashMap<MessageID, Message>>> = Data::new(Mutex::new(map));
+
+    let app_data = Data::new(Mutex::new(BTreeMap::<MessageID, Arc<Message>>::new()));
+
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .service(post_message)
             .service(get_messages)
-            .app_data(Data::clone(&msg_vec))
+            .app_data(Data::clone(&app_data))
     })
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
